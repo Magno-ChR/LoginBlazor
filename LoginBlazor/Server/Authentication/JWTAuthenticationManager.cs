8a72f4c4b1f60d37c5f29b0c3a0cfc3bbe6126c4
Carlo Magno Chuyes Romero <carlosgtmam@gmail.com>
2022-10-11 11:45:35 -0400

    Agregar archivos de proyecto.

---
+++ LoginBlazor/Server/Authentication/JWTAuthenticationManager.cs
@@ -0,0 +1,29 @@
+﻿using LoginBlazor.Shared;
+
+namespace LoginBlazor.Server.Authentication
+{
+    public class JWTAuthenticationManager
+    {
+        public const string JWT_SECURITY_KEY = "yPkHGAVHjavvASVAHvajVVGctgfhgCGCgsdgINvcsuAsd";
+        public const int JWT_TOKEN_VALIDITY_MINS = 20;
+
+        private UserAccountService _userAccountService;
+        public JWTAuthenticationManager(UserAccountService userAccountService)
+        {
+            _userAccountService = userAccountService;
+        }
+
+        public UserSession? GenerateJwtToken(string userName, string password)
+        {
+            if(string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+                return null;
+
+            /*Validando las credenciales del usuario*/
+
+            var userAccount = _userAccountService.GetUserAccountByUserName(userName);
+            if(userAccount == null || userAccount.Password != password)//si la contraseña no es valida o el nombre de usuario no existe devuelve null
+                return null;
+            /*Si las crewdenciales son valida*/
+        }
+    }
+}
